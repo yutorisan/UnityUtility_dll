08a9a1a2e76f5189ca627dfb01db10482ec0a853
yutori <>
2020-11-27 01:17:07 +0900

    UniReadOnly<T>にTへの暗黙的な変換を追加

--- UnityUnility/Classes/UniReadOnly.cs
+++ UnityUnility/Classes/UniReadOnly.cs
@@ -1,5 +1,11 @@
 ﻿using System;
-namespace UnityUtility.Classes
+using System.Collections.Generic;
+using UnityEngine.UIElements;
+using System.Collections.ObjectModel;
+using System.Collections.Immutable;
+using UnityEngine;
+
+namespace UnityUtility
 {
     /// <summary>
     /// 一度しか初期化できない値です。
@@ -8,7 +14,14 @@ namespace UnityUtility.Classes
     /// <typeparam name="T">ラップする型</typeparam>
     public class UniReadOnly<T>
     {
-        private bool m_isInitialized = false;
+        ///// <summary>
+        ///// 値を取得します。
+        ///// </summary>
+        public T Value { get; private set; }
+        /// <summary>
+        /// 初期化されているかどうかを取得します。
+        /// </summary>
+        public bool IsInitialized { get; private set; }
 
         /// <summary>
         /// 値を初期化します。このメソッドは一度しか呼ぶことができません。
@@ -17,21 +30,19 @@ namespace UnityUtility.Classes
         /// <exception cref="AlreadyInitializedException">複数回初期化しようとしたときにスローされます。</exception>
         public void Initialize(T value)
         {
-            if (m_isInitialized) throw new AlreadyInitializedException();
+            if (IsInitialized) throw new AlreadyInitializedException();
+            IsInitialized = true;
             Value = value;
-            m_isInitialized = true;
         }
 
-        /// <summary>
-        /// 値を取得します。
-        /// </summary>
-        public T Value { get; private set; }
+        public override string ToString() => Value.ToString();
+        public static implicit operator T(in UniReadOnly<T> readOnly) => readOnly.Value;
+    }
 
-        private class AlreadyInitializedException : Exception
+    internal class AlreadyInitializedException : Exception
+    {
+        public AlreadyInitializedException() : base("すでに初期化されています。")
         {
-            public AlreadyInitializedException() : base("すでに初期化されています。")
-            {
-            }
         }
     }
 }
---
+++ UnityUnility/Extensions/EnumExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace UnityUtility
+{
+    public static class EnumExtensions
+    {
+        /// <summary>
+        /// 列挙型のすべてにメンバを取得します。
+        /// </summary>
+        /// <typeparam name="TEnum"></typeparam>
+        /// <returns></returns>
+        public static IEnumerable<TEnum> GetEnumValues<TEnum>()
+        {
+            return Enum.GetValues(typeof(TEnum)).Cast<TEnum>();
+        }
+    }
+}
