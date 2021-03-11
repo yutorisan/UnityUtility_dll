39fd005a1ef7a28d97bcb4ede8457685fe743846
yutorisan <60573963+yutorisan@users.noreply.github.com>
2021-03-11 22:45:07 +0900

    LINQにFirstOrAnyオペレータを追加

---
+++ UnityUnility/Linq/Operators/Cache2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace UnityUtility.Linq
+{
+    //public static partial class LinqExtensions
+    //{
+
+    //}
+
+    //public interface ICachedEnumerable<T, TCache>
+    //{
+    //    IEnumerable<T> Sequence { get; }
+    //    TCache Cache { get; }
+    //}
+    //internal class CachedEnumerable<T, TCache> : ICachedEnumerable<T, TCache>
+    //{
+    //    public CachedEnumerable(IEnumerable<T> sequence, TCache)
+    //    {
+
+    //    }
+
+    //    public IEnumerable<T> Sequence => throw new NotImplementedException();
+
+    //    public TCache Cache => throw new NotImplementedException();
+    //}
+}
