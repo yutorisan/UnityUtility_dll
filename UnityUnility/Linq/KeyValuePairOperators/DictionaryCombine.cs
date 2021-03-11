443b9b4d0c82092b3868f8ee2d8fcd1e731a36c7
yutorisan <60573963+yutorisan@users.noreply.github.com>
2021-03-11 22:44:45 +0900

    DictionaryConbineで、デフォルト値として参照型を使うと1つのオブジェクトが共有されてしまうバグを修正

--- UnityUnility/Linq/KeyValuePairOperators/DictionaryCombine.cs
+++ UnityUnility/Linq/KeyValuePairOperators/DictionaryCombine.cs
@@ -8,7 +8,6 @@ namespace UnityUtility.Linq
     {
         /// <summary>
         /// キーが同一な2つの辞書型を指定された方法に従って合成し、新しい辞書型を生成します。
-        /// 片方にしかないキーが存在した場合、もう片方はdefaultとして合成します。
         /// </summary>
         /// <typeparam name="TKey"></typeparam>
         /// <typeparam name="TSourceValue1"></typeparam>
@@ -17,11 +16,15 @@ namespace UnityUtility.Linq
         /// <param name="source1"></param>
         /// <param name="source2"></param>
         /// <param name="resultSelector"></param>
+        /// <param name="defaultValue1Selector">source2のみに存在するキーがある場合に使用するsource1の値セレクター</param>
+        /// <param name="defaultValue2Selector">source1のみに存在するキーがある場合に使用するsource2の値セレクター</param>
         /// <returns></returns>
         public static IEnumerable<KeyValuePair<TKey, TResultValue>> DictionaryCombine<TKey, TSourceValue1, TSourceValue2, TResultValue>
             (this IEnumerable<KeyValuePair<TKey, TSourceValue1>> source1,
              IEnumerable<KeyValuePair<TKey, TSourceValue2>> source2,
-             Func<TSourceValue1, TSourceValue2, TResultValue> resultSelector)
+             Func<TSourceValue1, TSourceValue2, TResultValue> resultSelector,
+             Func<TSourceValue1> defaultValue1Selector,
+             Func<TSourceValue2> defaultValue2Selector)
         {
             if (source1 is null)
             {
@@ -37,12 +40,22 @@ namespace UnityUtility.Linq
             {
                 throw new ArgumentNullException(nameof(resultSelector));
             }
+
+            if (defaultValue1Selector is null)
+            {
+                throw new ArgumentNullException(nameof(defaultValue1Selector));
+            }
+
+            if (defaultValue2Selector is null)
+            {
+                throw new ArgumentNullException(nameof(defaultValue2Selector));
+            }
             //まずはsource1を精査
             foreach (var element1 in source1)
             {
                 TKey key1 = element1.Key;
                 TSourceValue1 value1 = element1.Value;
-                TSourceValue2 value2 = source2.FirstOrDefault(kvp => kvp.Key.Equals(key1)).Value;
+                TSourceValue2 value2 = source2.FirstOrAny(kvp => kvp.Key.Equals(key1), new KeyValuePair<TKey, TSourceValue2>(default, defaultValue2Selector())).Value;
 
                 yield return new KeyValuePair<TKey, TResultValue>(
                     key1,
@@ -56,9 +69,28 @@ namespace UnityUtility.Linq
                 {
                     yield return new KeyValuePair<TKey, TResultValue>(
                         element2.Key,
-                        resultSelector(default, element2.Value));
+                        resultSelector(defaultValue1Selector(), element2.Value));
                 }
             }
         }
+
+        /// <summary>
+        /// キーが同一な2つの辞書型を指定された方法に従って合成し、新しい辞書型を生成します。
+        /// 片方にしかないキーが存在した場合、もう片方はdefaultとして合成します。
+        /// </summary>
+        /// <typeparam name="TKey"></typeparam>
+        /// <typeparam name="TSourceValue1"></typeparam>
+        /// <typeparam name="TSourceValue2"></typeparam>
+        /// <typeparam name="TResultValue"></typeparam>
+        /// <param name="source1"></param>
+        /// <param name="source2"></param>
+        /// <param name="resultSelector"></param>
+        /// <returns></returns>
+        public static IEnumerable<KeyValuePair<TKey, TResultValue>> DictionaryCombine<TKey, TSourceValue1, TSourceValue2, TResultValue>
+            (this IEnumerable<KeyValuePair<TKey, TSourceValue1>> source1,
+             IEnumerable<KeyValuePair<TKey, TSourceValue2>> source2,
+             Func<TSourceValue1, TSourceValue2, TResultValue> resultSelector) =>
+                source1.DictionaryCombine(source2, resultSelector, () => default, () => default);
+
     }
 }
