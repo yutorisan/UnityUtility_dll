39125254a29ee653faa1b3c9d3038fa4e373dcc0
yutorisan <60573963+yutorisan@users.noreply.github.com>
2021-07-16 15:09:39 +0900

    WithIndexValue構造体をWithIndex構造体に名前変更 任意のオブジェクトをWithIndexにラップできる拡張メソッドを追加

--- UnityUnility/Linq/Core/WithIndexEnumerable.cs
+++ UnityUnility/Linq/Core/WithIndexEnumerable.cs
@@ -4,9 +4,9 @@ using System.Collections.Generic;
 
 namespace UnityUtility.Linq.Core
 {
-    public interface IWithIndexEnumerable<T> : IEnumerable<WithIndexValue<T>>
+    public interface IWithIndexEnumerable<T> : IEnumerable<WithIndex<T>>
     {
-
+        //なし
     }
 
     internal class WithIndexEnumerable<T> : IWithIndexEnumerable<T>
@@ -15,15 +15,27 @@ namespace UnityUtility.Linq.Core
 
         public WithIndexEnumerable(IEnumerable<T> source) => this.source = source;
 
-        public IEnumerator<WithIndexValue<T>> GetEnumerator()
+        public IEnumerator<WithIndex<T>> GetEnumerator()
         {
             int index = 0;
             foreach (var item in source)
             {
-                yield return new WithIndexValue<T>(item, index++);
+                yield return new WithIndex<T>(item, index++);
             }
         }
 
         IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+
+    public static class Extensions
+    {
+        /// <summary>
+        /// オブジェクトを<see cref="WithIndex{T}(T, int)"/>にラップする拡張メソッドです
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="value"></param>
+        /// <param name="index"></param>
+        /// <returns></returns>
+        public static WithIndex<T> WithIndex<T>(this T value, int index) => new WithIndex<T>(value, index);
     }
 }
--- UnityUnility/Linq/Core/WithIndexValue.cs
+++ UnityUnility/Linq/Core/WithIndexValue.cs
@@ -5,16 +5,18 @@ namespace UnityUtility.Linq.Core
     /// 値とインデックス番号のペア
     /// </summary>
     /// <typeparam name="T"></typeparam>
-    public readonly struct WithIndexValue<T> : IEquatable<WithIndexValue<T>>
+    public readonly struct WithIndex<T> : IEquatable<WithIndex<T>>
     {
-        public WithIndexValue(T value, int index) => (Value, Index) = (value, index);
+        public WithIndex(T value, int index) => (Value, Index) = (value, index);
         public T Value { get; }
         public int Index { get; }
         //分解メソッド
         public void Deconstruct(out T value, out int index) => (value, index) = (Value, Index);
 
-        public bool Equals(WithIndexValue<T> other) =>
+        public bool Equals(WithIndex<T> other) =>
             this.Value.Equals(other.Value) &&
             this.Index == other.Index;
     }
+
+    
 }
