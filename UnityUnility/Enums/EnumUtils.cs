6780f06133b2c5776ed9f51c45d9d74ebd8a83fa
yutorisan <yutorisan19@gmail.com>
2021-09-20 02:14:17 +0900

    Extensions/Enum→Enums/EnumUtilsにパスとファイル名の変更、及び名前空間の変更 （機能の実態に合わせ。拡張メソッドではないので）

--- UnityUnility/Enums/EnumUtils.cs
+++ UnityUnility/Enums/EnumUtils.cs
@@ -1,10 +1,9 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
-using UnityEngine;
 
-namespace UnityUtility.Extensions
+namespace UnityUtility.Enums
 {
-    public static class Enum
+    public static class EnumUtils
     {
         /// <summary>
         /// 列挙型のすべてのメンバを取得します。
