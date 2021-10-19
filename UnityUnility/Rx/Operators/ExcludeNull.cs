0c4aebda943adbcc8388974c64e3de731bf13898
yutorisan <yutorisan19@gmail.com>
2021-10-20 03:36:45 +0900

    4つのRxオペレータを追加

---
+++ UnityUnility/Rx/Operators/ExcludeNull.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UniRx;
+
+namespace UnityUtility.Rx.Operators
+{
+    public static partial class ObservableEx
+    {
+        /// <summary>
+        /// <code>Where(x => x != null)</code>の簡略記法
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="source"></param>
+        /// <returns></returns>
+        public static IObservable<T> ExcludeNull<T>(this IObservable<T> source) =>
+            source.Where(n => n != null);
+    }
+}
+
