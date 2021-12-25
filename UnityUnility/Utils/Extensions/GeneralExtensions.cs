9d0793e7613be452a75462adcbfd18eccfba78e9
yutorisan <yutorisan19@gmail.com>
2021-12-25 17:20:04 +0900

    TakeIfオペレータとLog拡張メソッド追加

---
+++ UnityUnility/Utils/Extensions/GeneralExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+namespace UnityUtility
+{
+    public static class GeneralExtensions
+    {
+        /// <summary>
+        /// このオブジェクトをログ出力します
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="obj"></param>
+        /// <param name="label"></param>
+        /// <returns></returns>
+        public static T Log<T>(this T obj, string label = null)
+        {
+            string log = label is null ? obj.ToString() : $"[{label}] {obj.ToString()}";
+            UnityEngine.Debug.Log(log);
+            return obj;
+        }
+    }
+}
+
