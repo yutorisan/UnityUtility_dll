24b03e7aae8fb82324ec41540d2af6e7c86383ec
yutorisan <yutorisan19@gmail.com>
2022-02-19 03:55:25 +0900

    SpriteRendererの大きさを一発変換する拡張メソッド追加

--- UnityUtility3/Utils/Extensions/UnityExtensions.cs
+++ UnityUtility3/Utils/Extensions/UnityExtensions.cs
@@ -12,5 +12,20 @@ namespace UnityUtility
         /// <param name="component"></param>
         /// <returns></returns>
         public static IEnumerable<Transform> Transforms(this Component component) => component.transform.Cast<Transform>();
+
+        /// <summary>
+        /// Spriteの大きさを指定のサイズに修正する
+        /// </summary>
+        /// <param name="renderer"></param>
+        /// <param name="size">このサイズに修正する。指定しなければ1x1</param>
+        public static void NormalizeSize(this SpriteRenderer renderer, Vector2? size = null)
+        {
+            size ??= Vector2.one;
+            float sizeX = renderer.bounds.size.x;
+            float sizeY = renderer.bounds.size.y;
+            float scaleX = size.Value.x / sizeX;
+            float scaleY = size.Value.y / sizeY;
+            renderer.gameObject.transform.localScale = new Vector2(scaleX, scaleY);
+        }
     }
 }
