3238dee69b6dd4e6df2347736c5449ecb9e1b0d4
yutorisan <yutorisan19@gmail.com>
2022-08-13 19:38:58 +0900

    ExcludeNullに新たなオプション追加

--- UnityUtility3/Rx/Operators/ExcludeNull.cs
+++ UnityUtility3/Rx/Operators/ExcludeNull.cs
@@ -13,6 +13,16 @@ namespace UnityUtility.Rx.Operators
         /// <returns></returns>
         public static IObservable<T> ExcludeNull<T>(this IObservable<T> source) =>
             source.Where(n => n != null);
+        /// <summary>
+        /// <code>Where(x => f(x) != null)</code>の簡略記法
+        /// </summary>
+        /// <typeparam name="TSource"></typeparam>
+        /// <typeparam name="TSelector"></typeparam>
+        /// <param name="source"></param>
+        /// <param name="selector"></param>
+        /// <returns></returns>
+        public static IObservable<TSource> ExcludeNull<TSource, TSelector>(this IObservable<TSource> source, Func<TSource, TSelector> selector) =>
+            source.Where(n => selector(n) != null);
     }
 }
 
